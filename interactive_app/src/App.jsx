451658a72aece999cea6ed91feb465d8b17369c4
Sudarshan-21 <slbondge@gmail.com>
2024-06-24 10:13:28 +0530

    setup corrections

--- interactive_app/src/App.jsx
+++ interactive_app/src/App.jsx
@@ -1,21 +1,18 @@
-import {
-  BrowserRouter,
-  Route,
-  Routes,
-} from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Signup } from "./pages/Signup";
+import { Signin } from "./pages/Signin";
 
 function App() {
   return (
     <>
-    <BrowserRouter>
+      <BrowserRouter>
         <Routes>
           <Route path="/signup" element={<Signup />} />
           <Route path="/signin" element={<Signin />} />
-          </Routes>
+        </Routes>
       </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
